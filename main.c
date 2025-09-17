d217574da5f8e9cd949d7ac6eb552d43841f1e0b
Thomas Robert <throbert@c2r6p4.42lehavre.fr>
2025-09-17 15:44:07 +0200

    Fix: Pretty much working

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: throbert <throbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 23:05:51 by throbert          #+#    #+#             */
-/*   Updated: 2025/09/12 00:46:17 by throbert         ###   ########.fr       */
+/*   Updated: 2025/09/17 15:43:16 by throbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,129 +16,108 @@ char	*get_path(char **env)
 {
 	int		i;
 	char	*to_find;
+	char	*result;
 
 	i = 0;
 	to_find = "PATH=";
 	while (env[i] != NULL)
 	{
-		if (ft_strcmp(to_find, &env[i][0]) == 0)
+		if (ft_strncmp(to_find, &env[i][0], 5) == 0)
 			break ;
 		i++;
 	}
-	ft_substr_end(&env[i][0], PATH_LENGTH);
-	return (&env[i][0]);
-}
-
-char	**get_cmd(char **av, pid_t pid)
-{
-	char	**cmd;
-
-	cmd = NULL;
-	if (pid == 0)
-	{
-		if (ft_strchr(av[2], ' '))
-			ft_split(av[2], ' ');
-		else
-		{
-			cmd = malloc(sizeof(char *) * 2);
-			if (!cmd)
-				return (NULL);
-			cmd[0] = alloc_cmd(&av[2][0]);
-			cmd[1] = NULL;
-		}
-	}
-	else
-	{
-		if (ft_strchr(av[3], ' '))
-			ft_split(av[3], ' ');
-		else
-		{
-			cmd = malloc(sizeof(char *) * 2);
-			if (!cmd)
-				return (NULL);
-			cmd[0] = alloc_cmd(&av[3][0]);
-			cmd[1] = NULL;
-		}
-	}
-	return (cmd);
+	result = ft_substr_end(&env[i][0], PATH_LENGTH);
+	return (result);
 }
 
 char	*get_abs_path(char *path, char *cmd)
 {
 	char	**to_try;
+	char	*merged_try;
 	int		i;
 
 	i = 0;
 	to_try = ft_split(path, ':');
 	if (to_try == NULL)
 		return (NULL);
+	cmd = ft_strjoin("/", &cmd[0]);
 	while (to_try[i] != NULL)
 	{
-		ft_strjoin(to_try[i], cmd);
-		if (access(to_try[i], X_OK) == 0)
+		merged_try = ft_strjoin(to_try[i], cmd);
+		if (access(merged_try, X_OK) == 0)
 			break ;
 		i++;
 	}
-	return (to_try[i]);
+	return (merged_try);
 }
 
-void	ft_exec(char *absolute_path, char **cmd, char **env, pid_t pid)
-{
-	int	pipefd[2];
-	int	pipe_success;
+// void	ft_exec(char *absolute_path, char **cmd, char **env)
+// {
 
-	pipe_success = pipe(pipefd);
-	if (pipe_success < 0)
-	{
-		free(cmd);
-		free(absolute_path);
-		exit(1);
-	}
-	if (pid != 0)
-	{
-		dup2(pipefd[0], STDIN_FILENO);
-		if (execve(absolute_path, cmd, env) == -1)
-		{
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		dup2(pipefd[1], STDOUT_FILENO);
-		if (execve(absolute_path, cmd, env) == -1)
-		{
-			perror("execve");
-			exit(EXIT_FAILURE);
-		}
-	}
-	close(pipefd[1]);
-}
+// }
+
+// pb si commande avec flag
 
 int	main(int ac, char **av, char **env)
 {
-	pid_t	pid;
+	pid_t	pid[2];
 	char	**cmd;
 	char	*path;
 	char	*absolute_path;
+	int		status;
+	int		pipefd[2];
+	int		pipe_success;
+	int		fds[2];
 
 	if (ac != 5)
 		return (ft_putstr_fd_int("Wrong number of arguments", 2));
 	path = get_path(env);
-	pid = fork();
-	cmd = get_cmd(av, pid);
-	absolute_path = get_abs_path(path, cmd[0]);
-	for (int i = 0; cmd[i] != NULL; i++)
-		printf("cmd : %s\n", cmd[i]);
-	if (pid != 0)
+	pipe_success = pipe(pipefd);
+	if (pipe_success < 0)
 	{
-		ft_redirect("STDIN TO INFILE", av[1]);
-		ft_exec(absolute_path, cmd, env, pid);
+		free(path);
+		exit(1);
 	}
-	else
+	pid[0] = fork();
+	if (!pid[0])
 	{
-		ft_redirect("STDOUT TO OUTFILE", av[4]);
-		ft_exec(absolute_path, cmd, env, pid);
+		fds[1] = open(av[1], O_RDONLY);
+		dup2(fds[1], STDIN_FILENO);
+		close(fds[1]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		close(pipefd[0]);
+		cmd = ft_split(av[2], ' ');
+		absolute_path = get_abs_path(path, ft_strjoin("/", cmd[0]));
+		if (execve(absolute_path, cmd, env) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
 	}
-	return (0);
+	waitpid(pid[0], &status, 0);
+	status = WEXITSTATUS(status);
+	pid[1] = fork();
+	if (!pid[1])
+	{
+		fds[0] = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0755);
+		dup2(fds[0], STDOUT_FILENO);
+		close(fds[0]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		cmd = ft_split(av[3], ' ');
+		absolute_path = get_abs_path(path, ft_strjoin("/", cmd[0]));
+		if (execve(absolute_path, cmd, env) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	while (waitpid(-1, &status, 0) == -1)
+		;
+	status = WEXITSTATUS(status);
+	return (status);
 }
